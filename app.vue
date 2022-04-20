bc15c0c2b6fa61843cd8f994ed048dc094bc1635
Yuichi Takebe <takebe.yuichi@gmail.com>
2022-04-21 00:04:34 +0900

    update

--- app.vue
+++ app.vue
@@ -8,10 +8,6 @@ useHead({
   link: [
     { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
     {
-      rel: 'stylesheet',
-      href: '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/styles/xcode.min.css',
-    },
-    {
       rel: 'apple-touch-icon',
       type: 'image/png',
       href: '/apple-touch-icon.png',
