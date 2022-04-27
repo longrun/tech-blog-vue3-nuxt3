7c98702e647f765a7dcaa623aa7d88cff2c7746d
Yuichi Takebe <takebe.yuichi+gh@gmail.com>
2022-04-27 21:57:42 +0900

    Feat/visible static image (#17)
    * fixed #16
    
    * 0.1.1-beta.0
    
    Co-authored-by: Yuichi Takebe <takebe.yuichi@gmail.com>

--- plugins/contentful.ts
+++ plugins/contentful.ts
@@ -8,7 +8,6 @@ import contentful from 'contentful'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
-
   const client = contentful.createClient({
     space: config.private.CONTENTFUL_SPACE_ID,
     accessToken: config.private.CONTENTFUL_ACCESS_TOKEN,
