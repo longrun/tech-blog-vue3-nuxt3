1179104345e2a055c7a793846b4ce097ad421717
Yuichi Takebe <takebe.yuichi+gh@gmail.com>
2022-04-29 14:35:32 +0900

    fix (#18)
    Co-authored-by: Yuichi Takebe <takebe.yuichi@gmail.com>

--- plugins/contentful.ts
+++ plugins/contentful.ts
@@ -1,14 +1,15 @@
 import { defineNuxtPlugin } from '#app'
 
+// for dev
+import { createClient } from 'contentful'
+
 // for SSR, SSG
 import contentful from 'contentful'
 
-// for dev
-// import * as contentful from 'contentful'
-
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
-  const client = contentful.createClient({
+  const createClientFunc = process.env.NODE_ENV === 'development' ? createClient : contentful.createClient
+  const client = createClientFunc({
     space: config.private.CONTENTFUL_SPACE_ID,
     accessToken: config.private.CONTENTFUL_ACCESS_TOKEN,
   })
